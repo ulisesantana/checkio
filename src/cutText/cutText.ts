ec281cb182c2d0bc8f4dbe69acd28eeb7f1643fb
Ulises Santana <ulisesantana@gmail.com>
2017-10-26 21:34:18 +0100

    Refactor lastCharIsSpace

--- src/cutText/cutText.ts
+++ src/cutText/cutText.ts
@@ -1,26 +1,26 @@
 export default function cutText(numOfChars: number, text: string): string {
   let words: string[] = text.split(" ");
-  let dots: string = "..."
+  let dots: string = "...";
   let output: string = getOutput(numOfChars, text);
-  
-  if (numOfChars >= text.length && text.length !== 0){
+
+  if (numOfChars >= text.length && text.length !== 0) {
     return text
   }
-  
+
   while (!isDone(output, words[0])) {
-    
+
     if (numOfChars === 0) {
       return getOutput(numOfChars, text).concat(dots);
     }
-    
+
     numOfChars--;
     output = getOutput(numOfChars, text);
   }
-  
-  if (lastCharIsASpace(output, words[0])) {
+
+  if (lastCharIsASpace(output)) {
     numOfChars--;
   }
-  
+
   return getOutput(numOfChars, text).concat(dots);
 }
 
@@ -31,13 +31,13 @@ function getOutput(numOfChars: number, text: string): string {
 }
 
 function isDone(output: string, firstWord: string): boolean {
-  return isTheFirstWord(output, firstWord) || lastCharIsASpace(output, firstWord);
+  return isTheFirstWord(output, firstWord) || lastCharIsASpace(output);
 }
 
 function isTheFirstWord(output: string, firstWord: string): boolean {
   return output.length === firstWord.length;
 }
 
-function lastCharIsASpace(output: string, firstWord: string): boolean {
+function lastCharIsASpace(output: string): boolean {
   return output[output.length - 1] === " ";
 }
